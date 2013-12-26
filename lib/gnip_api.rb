9443133a03dc69827f2cd7d2ccfedfe6376dc531
Amar Daxini <amardaxini@gmail.com>
2013-12-26 16:30:11 +0530

    added require

--- lib/gnip_api.rb
+++ lib/gnip_api.rb
@@ -5,10 +5,10 @@ require 'multi_json'
 require 'oj'
 require 'typhoeus/adapters/faraday'
 require 'pry'
-require "./gnip_api/version"
-require "./gnip_api/configuration"
-require "./gnip_api/errors"
-require "./gnip_api/search"
+require "gnip_api/version"
+require "gnip_api/configuration"
+require "gnip_api/errors"
+require "gnip_api/search"
 
 module GnipApi
   # Your code goes here...
